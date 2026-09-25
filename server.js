be5680a9e3403b69b4ff038c3e9d2a3be8e603c2
devin3394-maker <devin3394@yahoo.com>
2026-09-25 01:21:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ Include:
     });
   } catch (error) {
     console.error(error);
-
+console.error(error?.response?.data || error);
     res.status(500).json({
       error: "Failed to generate episode",
     });
